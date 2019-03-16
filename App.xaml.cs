46dc80f19d1992c01696f63390d7f09d80f0be98
Itamar Asis <ita.asis@gmail.com>
2019-03-16 14:34:50 +0200

    AppUpdateManager SSL / TLS bugfix

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,6 +20,10 @@ namespace PlayLogger
         [STAThread]
         public static void Main()
         {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            // Use SecurityProtocolType.Ssl3 if needed for compatibility reasons
+
             updateManager = AppUpdateManager.Instance;
 
             var application = new App();
